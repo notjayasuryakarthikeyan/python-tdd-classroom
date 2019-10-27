849bd71f893ae373540beac6d209ad3f53fb5261
unknown <alam.shams.1996@gmail.com>
2019-10-27 22:13:45 +0530

    Implement function

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,24 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    start_index = 0
+    end_index = len(input_list)-1
+    while start_index < end_index:
+        t = input_list[start_index]
+        input_list[start_index] = input_list[end_index]
+        input_list[end_index] = t
+        start_index +=1
+        end_index -=1
+
+    return input_list
+
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    count_digit = 0
+    while number:
+        number = number//10
+        count_digit += 1
+    return count_digit
--- src/string_exercise.py
+++ src/string_exercise.py
@@ -7,25 +7,59 @@ class StringExercise:
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        reverse_str = ""
+        for char_of_str in input_str[::-1]:
+            reverse_str += char_of_str
+
+        return reverse_str
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        return None
+        english_vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        if character in english_vowel:
+            return True
+
+        return False
 
     def find_longest_word(self, sentence):
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        longest_word = ""
+        max_length = 0
+        word = ""
+
+        for index_of_each_char in range(len(sentence)):
+            char_of_sentence = sentence[index_of_each_char]
+            if char_of_sentence != ' ':
+                word = word + char_of_sentence
+
+            if char_of_sentence == ' ' or index_of_each_char == len(sentence)-1:
+                if max_length < len(word):
+                    max_length = len(word)
+                    longest_word = word
+                word = ""
+
+        return longest_word
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+        word = ""
+        word_length_list = []
+        for index_of_each_char in range(len(text)):
+            char_of_text = text[index_of_each_char]
+            if char_of_text != ' ':
+                word = word + char_of_text
+
+            if char_of_text == ' ' or index_of_each_char == len(text) - 1:
+                word_length_list.append(len(word))
+                word = ""
+
+        return word_length_list
